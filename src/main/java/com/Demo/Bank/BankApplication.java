d338b73227b1eb65e77df6c00fd562321396dbfd
manikandan-704 <nagarajpriyan2004@gmail.com>
2026-04-22 20:30:28 +0530

    BankApplication Modified

--- src/main/java/com/Demo/Bank/BankApplication.java
+++ src/main/java/com/Demo/Bank/BankApplication.java
@@ -2,7 +2,6 @@ package com.Demo.Bank;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @SpringBootApplication
 public class BankApplication {
